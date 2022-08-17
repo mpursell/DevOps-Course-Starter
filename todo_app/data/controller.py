f4eb2b1b5eb9eb91f766ea3a165f8ee21ca02b9c
Mike <michael.pursell@gmail.com>
2022-08-17 20:55:31 +0100

    working on add_Task method for MongoDB

--- todo_app/data/controller.py
+++ todo_app/data/controller.py
@@ -4,10 +4,6 @@ import requests
 import os
 import pymongo
 
-
-mongodbConnectionString = os.environ.get("MONGO_CONN_STRING")
-applicationDatabase = os.environ.get("MONGO_DB_NAME")
-
 class DatabaseAbstract(ABC):
 # interface to describe what our database class must implement
     
@@ -20,28 +16,14 @@ class DatabaseAbstract(ABC):
 
     def connectDatabase(connectionString):
         pass
-
-    def getCollections():
-        pass
-    
-    def getItemsInCollection(collectionName):
-        pass
-    
-    def addItemToCollection(itemName, targetCollectionName):
-        pass
-
-    def moveItemToCollection(itemName, targetCollectionName):
-        pass
-    
+   
 
 class AppDatabase(DatabaseAbstract):
-
 
     def __init__(self, connectionString) -> None:
     
         self._connectionString = connectionString
         
-
     @property
     def databaseName(self):
         return self._databaseName
@@ -66,40 +48,13 @@ class AppDatabase(DatabaseAbstract):
         applicationDB = client[databaseName] 
         return applicationDB        
 
-    # Get collections -> list of collections
-    def getCollections(self) -> list:
-        #database = self.connectDatabase()
-        collectionsInDatabase = self.list_collection_names()
-        return collectionsInDatabase
+class ViewModel:
+    def __init__(self, items):
+        self._items = items
 
-
-    # Get items in collection -> pymongo iterable cursor object
-    def getItemsInCollection(self):
-        database = self.connectDatabase().self_.collectionName
-        collection = database[collection]
-        
-        documentList = collection.find()
-        return documentList
-            
-
-    # Add item to collection -> item in new collection
-    def addItemToCollection(self, toDoItem, targetCollectionName):
-        database = self.connectDatabase(self._connectionString).self_.collectionName
-        existingCollectionsList = self.getCollections()
-
-        # check if the collection name exists already because if it doesn't
-        # mongoDB will just create a new collection, and we don't want to create
-        # collections outside of the already specified collections
-        if targetCollectionName in existingCollectionsList:
-            collection = database[targetCollectionName]
-            collection.insert_one(toDoItem)
-            
-            newItem = collection.find_one(toDoItem)
-            return newItem
-        else:
-            return "collection not in the existing list of collections"
-        
-
-    # Move item to collection -> item in new collection
+    @property
+    def items(self):
+        return self._items
+    
 
     
