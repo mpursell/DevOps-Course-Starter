5226bf2cd014aa455fa65450e68f5fa8c4e9edb1
Mike <michael.pursell@gmail.com>
2022-09-05 21:37:27 +0100

    editing container entrypoint shell scripts with new poetry location

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,4 +2,4 @@
 
 cd /usr/src/todo_app/todo_app
 
-$HOME/.poetry/bin/poetry run gunicorn --bind 0.0.0.0:5000 --workers=2 'app:create_app()'
+/root/.local/bin/poetry run gunicorn --bind 0.0.0.0:5000 --workers=2 'app:create_app()'
