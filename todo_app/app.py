8ebe8ee72d4d921f0a671fe0e60652e557ce3e44
Mike <michael.pursell@gmail.com>
2022-10-03 09:57:31 +0100

    Revert "black linter run"
    This reverts commit 9c353f556c08083b6ef29291a27eeece8c489008.

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,14 +1,14 @@
+from csv import writer
 from flask import Flask, abort
 from flask import render_template
 from flask import request
-from flask_login import LoginManager, login_required, login_user
 
+from werkzeug.utils import redirect
 from todo_app.flask_config import Config
 from todo_app.data.db_service import *
 from todo_app.data.user import User, check_role, writer_required, reader_required
 from todo_app.data.viewmodel import ViewModel
-
-from werkzeug.utils import redirect
+from flask_login import LoginManager, login_required, login_user
 
 import logging
 import os
@@ -29,19 +29,18 @@ def create_app():
             format="%(asctime)s %(levelname)s %(name)s %(threadName)s: %(message)s",
         )
 
-    # OAuth Login
+    #OAuth Login
     login_manager = LoginManager()
 
     @login_manager.unauthorized_handler
     def unauthenticated():
         # logic to redirect to GH auth flow if unauthed
 
-        client_id: str = os.environ.get("GH_CLIENTID")
-        callback_uri = os.environ.get("CALLBACK_URI")
-
-        return redirect(
-            f"https://github.com/login/oauth/authorize?client_id={client_id}&redirect_uri={callback_uri}"
-        )
+        client_id: str = os.environ.get('GH_CLIENTID')
+        callback_uri = os.environ.get('CALLBACK_URI')
+ 
+        return redirect(f'https://github.com/login/oauth/authorize?client_id={client_id}&redirect_uri={callback_uri}')
+        
 
     @login_manager.user_loader
     def load_user(user_id: str) -> User:
@@ -50,12 +49,14 @@ def create_app():
 
     login_manager.init_app(app)
 
+
     # MongoDB connection and setup
     mongodbConnectionString = os.environ.get("MONGO_CONN_STRING")
     applicationDatabase = os.environ.get("MONGO_DB_NAME")
 
     todo = AppDatabase(mongodbConnectionString).connectDatabase(applicationDatabase)
 
+    
     @app.route("/")
     @login_required
     def index():
@@ -110,44 +111,42 @@ def create_app():
         )
         return redirect("/")
 
-    @app.route("/login/callback", methods=["GET", "POST"])
+    @app.route('/login/callback', methods=['GET', 'POST'])
+
     def authenticate():
 
-        returned_github_code: str = request.args.get("code")
-        client_id: str = os.environ.get("GH_CLIENTID")
-        client_secret: str = os.environ.get("GH_ClIENTSECRET")
-
-        fetch_token_headers: dict = {"Accept": "application/json"}
+        returned_github_code: str = request.args.get('code')
+        client_id: str = os.environ.get('GH_CLIENTID')
+        client_secret: str = os.environ.get('GH_ClIENTSECRET')
+        
+        fetch_token_headers: dict = {
+            'Accept' : 'application/json'
+        }
 
         fetch_token_params: dict = {
-            "client_id": client_id,
-            "client_secret": client_secret,
-            "code": returned_github_code,
+            'client_id' : client_id,
+            'client_secret' : client_secret,
+            'code' : returned_github_code
         }
 
-        access_token_response = requests.post(
-            "https://github.com/login/oauth/access_token",
-            headers=fetch_token_headers,
-            params=fetch_token_params,
-        )
-        access_token: str = access_token_response.json()["access_token"]
+        access_token_response = requests.post('https://github.com/login/oauth/access_token', headers=fetch_token_headers, params=fetch_token_params)
+        access_token: str = access_token_response.json()['access_token']
 
         fetch_user_headers: dict = {
-            "Accept": "application/vnd.github+json",
-            "Authorization": f"Bearer {access_token}",
+            'Accept' : 'application/vnd.github+json',
+            'Authorization' : f'Bearer {access_token}'
         }
 
-        github_user = requests.get(
-            "https://api.github.com/user", headers=fetch_user_headers
-        ).json()
-
-        app_user = User(id=github_user["id"])
+        github_user = requests.get('https://api.github.com/user', headers=fetch_user_headers).json()
+  
+        
+        app_user = User(id = github_user['id'])
 
         login_user(app_user)
 
-        if check_role(app_user.id, "reader") == "reader":
+        if check_role(app_user.id, 'reader') == 'reader':
             return redirect("/")
         else:
-            abort(403)
+            abort(403) 
 
     return app
