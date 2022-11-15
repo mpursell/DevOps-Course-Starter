efb7e07a7ed19d2a3bbd386149af69332a68fc29
Michael Pursell <michael.pursell@gmail.com>
2022-11-15 19:59:09 +0000

    Update todo_app/app.py
    Co-authored-by: JackMead <Jack.Mead@softwire.com>

--- todo_app/app.py
+++ todo_app/app.py
@@ -127,7 +127,7 @@ def create_app():
         taskId = request.args.get("taskId")
         task = app_db.get_item(taskId)
 
-        return render_template("task.html", task=task, taskId=task.id, user=app_user)
+        return render_template("task.html", task=task, taskId=task.id, user=current_user)
 
     @writer_required
     @app.route("/update/", methods=["GET", "PUT"])
